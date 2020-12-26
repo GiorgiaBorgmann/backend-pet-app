1ac48b457c5c3539d2696d050eaacb659bf93030
Giorgia Borgmann <66149711+GiorgiaBorgmann@users.noreply.github.com>
2020-12-26 12:37:28 +0200

    register and login working

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 //import route
-
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts')
 dotenv.config();
 //connect to DB
 mongoose.connect(
@@ -16,9 +17,11 @@ mongoose.connect(
 
 //Middlewars 
 app.use(express.json())
+app.use(cors())
 
 //route MiddleWar 
 
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute)
 
 app.listen(4000, () => console.log('server up and running'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,52 @@
 const router = require('express').Router();
 const User = require('../model/User')
-
-
-//VALIDATION
-const Joi = require('@hapi/joi');
-const schema = {
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-}
-
+const { registerValidation, logInValidation } = require('../validation')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 router.post('/register', async (req, res) => {
-
-    //lets VALIDATE the data before make a user 
-    const { error } = Joi.validate(req.body, schema)
+    const { error } = registerValidation(req.body)
     if (error) {
         return res.status(400).send(error.details[0].message)
     }
+    //check if the user already exist in the db
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) {
+        return res.status(400).send('Email already exists')
+    }
+    //hash passwords 
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    //creat user
     const user = new User({
         name: req.body.name,
+        lastName: req.body.lastName,
+        phone: req.body.phone,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     })
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.send({ user: user._id })
     } catch (err) {
         res.status(400).send(err)
     }
 });
+router.post('/login', async (req, res) => {
+    const { error } = logInValidation(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(400).send('Email does not exists')
+    }
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) {
+        return res.status(400).send('Invalid password')
+    }
+    //creat token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
 
-
-
+})
 module.exports = router;
